f0d01f9a7a2d2adc349e9d4c6b009342cd01faf0
POEMN\ksy01 <ksy011212@naver.com>
2023-10-03 18:48:03 +0900

    [JAVA] 스레드

---
+++ myweb4/src/threadtest/BeepPrintExample.java
@@ -0,0 +1,25 @@
+package threadtest;
+
+import java.awt.Toolkit;
+
+public class BeepPrintExample {
+
+	public static void main(String[] args) {
+		Thread thread = new Thread() {
+			@Override
+			public void run() {
+			Toolkit toolkit = Toolkit.getDefaultToolkit();
+				for(int i=0; i<5; i++) {
+				toolkit.beep();
+				try { Thread.sleep(500); } catch(Exception e) {}
+				}
+			}
+		};
+		thread.start(); ;
+		for(int i=0; i<5; i++) {
+			System.out.println("띵");
+			try { Thread.sleep(500); } catch(Exception e) {}
+		}
+	}
+
+}
